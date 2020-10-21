14fc6721073cf941005154c5b8ba875d7a30806a
Zelenskiyas <zelensk1973@gmail.com>
2020-10-21 21:39:40 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let b = true;
+    let k = [];
+    for (let j=0; j<bracketsConfig.length; j++){
+      k[j] = 0;
+    }
+
+    for (let i=0; i<str.length; i++){
+      for (let j=0; j<bracketsConfig.length; j++){
+        brackL = bracketsConfig[0];
+        brackR = bracketsConfig[1];  
+        if (str[i] == brackL){
+          k[j]++;
+        }
+        if (str[i] == brackR){
+          k[j]--;
+          if (k[j] < 0){
+            return false;
+          }
+        }
+      }
+      for (let j=0; j<bracketsConfig.length; j++){
+        if (k[j] != 0){
+          return false;
+        }
+      }
+    }
+
+   
+    return true;
 }
