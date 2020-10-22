3d8e4fe4708811cd58bd6340df86d37b94a9f4f4
Zelenskiyas <zelensk1973@gmail.com>
2020-10-22 18:58:44 +0300

    fix: do 17 tests

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ module.exports = function check(str, bracketsConfig) {
     for (let j=0; j<bracketsConfig.length; j++){
       brackL = bracketsConfig[j][0];
       brackR = bracketsConfig[j][1];  
-      if (str[i] === brackL){
-          k.push(brackL)
-          // console.log("L--->",brackL);
-      } else 
-      if (str[i] === brackR){
-          // console.log("R--->",brackR);
-          let x = k.pop()
-      }        
+      if (brackL !== brackR){
+        if (str[i] === brackL){
+            k.push(brackL)
+            // console.log("L--->",brackL);
+        } else 
+        if (str[i] === brackR){
+            // console.log("R--->",brackR);
+            let x = k.pop()
+            if (x != brackL) return false;
+        } 
+    } else {
+        
+    }       
     }
   }     
   if (k.length > 0) return false;
