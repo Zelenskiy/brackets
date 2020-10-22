541f6b12d7b5a289b100609f39f0b2c689d0d3ae
Zelenskiyas <zelensk1973@gmail.com>
2020-10-22 08:17:30 +0300

    fix: attempt fix errors

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
     let b = true;
     let k = [];
-    for (let j=0; j<bracketsConfig.length; j++){
-      k[j] = 0;
-    }
+    // for (let j=0; j<bracketsConfig.length; j++){
+    //   k[j] = 0;
+    // }
 
     for (let i=0; i<str.length; i++){
       for (let j=0; j<bracketsConfig.length; j++){
         brackL = bracketsConfig[0];
         brackR = bracketsConfig[1];  
-        if (str[i] == brackL){
-          k[j]++;
-        }
-        if (str[i] == brackR){
-          k[j]--;
-          if (k[j] < 0){
-            return false;
+        if (true){//brackL !== brackB){
+          if (str[i] == brackL){
+            k.push(brackL)
           }
-        }
-      }
-      for (let j=0; j<bracketsConfig.length; j++){
-        if (k[j] != 0){
-          return false;
-        }
-      }
-    }
+          if (str[i] == brackR){
+            let x = k.pop()
+            if (x != brackR){
+              return false;
+            }
+          }
+        } else {
 
+        }
+      }
+      if (k.length > 0) return false;
+    }
+    if (k.length > 0) return false;
    
     return true;
 }
